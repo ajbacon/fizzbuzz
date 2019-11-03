127fba407e9e5b2a2646ceb382bf0c83e9092d2c
bensheridanedwards <bensheridanedwards@gmail.com>
2019-11-03 13:04:54 +0000

    Add fizzbuzz test to fizzbuzz_spec.rb

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,11 +1,19 @@
 require_relative "../lib/fizzbuzz"
 
-describe 'fizzbuzz' do 
+describe 'fizzbuzz' do
+  it 'returns "fizzbuzz" when passed a multiple of 3 & 5' do
+    expect(fizzbuzz(15)).to eq "fizzbuzz"
+  end
+
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
 
   it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
+  end
+
+  it 'returns number when passed a number that isn\'t divisible by 3 or 5' do
+    expect(fizzbuzz(2)).to eq 2
   end
 end
